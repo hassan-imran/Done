b8a70e32e2ccb9a06441f70dce1ba222cda4efef
hassan-imran <43490649+hassan-imran@users.noreply.github.com>
2018-09-25 11:14:35 +0500

    Some changes
    Added login as guest and trying to figure out how to write to external file so as to create a database like thingy

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 
-var users = ["hassan","guest"];
-var passes = ["Hassan123","Guest123"];
+var users = ["hassan", "guest"];
+var passes = ["Hassan123", "Guest123"];
 
-function login(a,b){
-    for(var i=0; i<users.length;i++){
-        if((a.toLowerCase()==users[i])&&(b==passes[i])){
+function login(a, b) {
+    for (var i = 0; i < users.length; i++) {
+        if ((a.toLowerCase() == users[i]) && (b == passes[i])) {
             document.getElementById("login").style = "display:none;";
             document.getElementById("internal").style = "display:block;";
         }
-        else{
+        else {
             document.getElementById("notValid").style = "display:block;";
         }
     }
 }
 
-function logOut(){
+function guest() {
+    login("guest","Guest123");
+}
+
+function logOut() {
     document.getElementById("internal").style = "display:none;";
     document.getElementById("login").style = "display:block;";
     document.getElementById("notValid").style = "display:none;";
 }
+function addIt() {
+    document.getElementById("adding").style.display = "block";
 
-var reminders=[];
+}
 
-if (reminders.length == 0){
-    document.getElementById("reminder").innerHTML += "You have no reminder";
+var reminders = [];
+
+if (reminders.length == 0) {
+    document.getElementById("reminder").innerHTML += "<br><br>You have no reminder";
 }

21fc3a7db3d164a724af45392951d0ae0996c865
hassan-imran <43490649+hassan-imran@users.noreply.github.com>
2018-10-03 22:57:27 +0500

    Solved the login bug

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ var users = [['guest', 'Guest123'], ['admin', 'Admin123']];
 function verify() {
     var a = document.getElementById("userName").value;
     var b = document.getElementById("userPass").value;
-    var x = 0;
+    var x = false;
     for (var i = 0; i < users.length; i++) {
-        if (users[i][0] === a && users[i][1] === b) {
+        if (users[i][0].toLowerCase() === a.toLowerCase() && users[i][1] === b) {
             var accountKey = i;
             localStorage.setItem("account", accountKey);
             window.location.assign("home.html");
-        }
-        else {
-            x = -1;
+            x=true;
+            break;
         }
     }
-    if (x === -1) {
+    
+    if (!x) {
         document.getElementById("invalid").style.display = "block";
         document.getElementById("userPass").value = "";
     }

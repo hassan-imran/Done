835d93f397a728bab6cf6130f44172d0d6e63b75
hassan-imran <43490649+hassan-imran@users.noreply.github.com>
2018-10-03 16:15:31 +0500

    Totally modified & new app
    With a securrity verification feature too

--- script.js
+++ script.js
@@ -1,73 +1,24 @@
 
-var users = ["hassan", "guest"];
-var passes = ["Hassan123", "Guest123"];
+var users = [['guest', 'Guest123'], ['admin', 'Admin123']];
 
-function login(a, b) {
+function verify() {
+    var a = document.getElementById("userName").value;
+    var b = document.getElementById("userPass").value;
+    var x = 0;
     for (var i = 0; i < users.length; i++) {
-        if ((a.toLowerCase() == users[i]) && (b == passes[i])) {
-            document.getElementById("login").style = "display:none;";
-            document.getElementById("internal").style = "display:block;";
+        if (users[i][0] === a && users[i][1] === b) {
+            var accountKey = i;
+            window.location.assign("home.html");
+            localStorage.setItem("account", accountKey);
         }
         else {
-            document.getElementById("notValid").style = "display:block;";
+            x = -1;
         }
     }
-}
-
-function guest() {
-    login("guest", "Guest123");
-}
-
-function logOut() {
-    document.getElementById("internal").style = "display:none;";
-    document.getElementById("login").style = "display:block;";
-    document.getElementById("notValid").style = "display:none;";
-}
-function addIt() {
-    document.getElementById("adding").style.display = "block";
-
-}
-
-function addRemind() {
-    var a = document.getElementById("remindTitle").value;
-    var b = document.getElementById("remindTime").value;
-    var c = document.getElementById("remindMsg").value;
-
-    if (a && b) {
-        reminders.push([a, b, c]);
-        update();
-    }
-    else {
-        document.getElementById("remindTitle").style.boxShadow = "0 0 5px red";
-        document.getElementById("remindTime").style.boxShadow = "0 0 5px red";
+    if (x === -1) {
+        document.getElementById("invalid").style.display = "block";
+        document.getElementById("userPass").value = "";
     }
 }
 
-function update() {
-    document.getElementById("adding").style.display = "none";
-    if (reminders.length == 0) {
-        document.getElementById("reminderTable").style.display = "none";
-        document.getElementById("noReminder").style.display = "block";
-    }
 
-    else {
-        document.getElementById("reminderTable").style.display = "block";
-        document.getElementById("reminderTableBody").innerHTML = "";
-        document.getElementById("noReminder").style.display = "none";
-
-        for (var i = 0; i < reminders.length; i++) {
-            document.getElementById("reminderTableBody").innerHTML += "<tr id=row" + i + "><td>" + reminders[i][0] + "</td><td>" + reminders[i][1] + "</td><td>" + reminders[i][2] + "</td><td><button onclick='edit(" + i + ")'>Edit</button><button onclick='del(" + i + ")'>Delete</button></td></tr>";
-        }
-    }
-}
-
-function del(x){
-    reminders.splice(x,1);
-    update();
-}
-
-function edit(x){
-}
-
-var reminders = [];
-update();
